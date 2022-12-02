6d2bfdb92253acb37dad82a5c3a460f6c6baeb82
hiramr97 <demonic2568@gmail.com>
2022-12-02 14:57:15 +0000

    added some

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './Compliment';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{data[Math.floor(Math.random * data.length)]}</h1>
     </div>
   );
 }
